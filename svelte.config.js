c9ced757210f504eade4871a24078edcf6c370a1
merryt <tymerry@gmail.com>
2022-08-01 10:17:23 -0600

    hopes and dreams

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,7 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			edge: false,
-			split: false
-		}),
+		adapter: adapter(),
 	},
 
 	preprocess: [
